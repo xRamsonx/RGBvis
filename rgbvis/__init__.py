834015341dc5a727ba4f98c42be5cf2820f747b3
Kai Arnold <kai.arnold42@gmail.com>
2024-03-19 21:59:03 +1100

    added init for pip install

---
+++ rgbvis/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 19 11:07:30 2024
+
+@author: kai
+"""
+from rgbvis.sequence import Sequence
+from rgbvis.section import Section
