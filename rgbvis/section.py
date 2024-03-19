4df49e2cb486a7b986c9dd129544fc683c911b5a
Kai Arnold <kai.arnold42@gmail.com>
2024-03-19 23:16:20 +1100

    Added doc

--- rgbvis/section.py
+++ rgbvis/section.py
@@ -1,4 +1,17 @@
 class Section:
+    """
+    Represents a section of a visualization with a specific type, end value, and intensity.
+
+    Attributes:
+        type (str): The type of the section ("rising", "sinking", or "linear").
+        endvalue (float): The end value of the section (between 0 and 1).
+        intensity (float): The intensity of the section (default is 1.0).
+
+    Methods:
+        evaluate(time): Evaluates the section at a given time (0-1).
+
+    """
+
     def __init__(self, type, endvalue, intensity=1.0):
         self.type = type
         self.endvalue = endvalue
@@ -9,7 +22,16 @@ class Section:
         self.intensity = intensity
     
     def evaluate(self, time):
-        # Evaluate the section at a given time (0-1)
+        """
+        Evaluates the section at a given time.
+
+        Args:
+            time (float): The time value (between 0 and 1).
+
+        Returns:
+            float: The evaluated value of the section at the given time.
+
+        """
         if self.type == "rising":
             return time**(3/self.intensity) 
         elif self.type == "sinking":
--- rgbvis/sequence.py
+++ rgbvis/sequence.py
@@ -3,10 +3,41 @@ from plotly.subplots import make_subplots
 import plotly.graph_objects as go
 
 class Sequence:
+    """
+    A class representing a sequence of color changes.
+
+    Attributes:
+    - sequence: A list of lists representing the sequence of color changes.
+                The inner lists contain the duration and section information for each color channel.
+
+    Methods:
+    - add(type, duration, endvalue, rgbs="rgb", intensity=1.0): Adds a color change section to the sequence.
+    - evaluate(time): Evaluates the color values at a given time.
+    - get_values(x): Returns the color values for a list of time points.
+    - lines(x): Returns a list of Scatter objects for plotting the color change lines.
+    - plot(x): Returns a Figure object for plotting the color change lines.
+    - to_binary(rgb): Converts the color values to binary format.
+    - create_frames(x, size): Creates frames for animation.
+    - create_frame2(x): Creates frames for the second subplot in the animation.
+    - animate(x, fps=30, size=250): Creates an animated plot of the color changes.
+    - show(x): Creates a subplot with the color change lines and the animated plot.
+    """
+
     def __init__(self):
         self.sequence = [[],[],[]] # red, green, blue
     
     def add(self, type, duration, endvalue, rgbs="rgb", intensity=1.0):
+        """
+        Adds a color change section to the sequence.
+
+        Parameters:
+        - type: The type of color change section.
+        - duration: The duration of the color change section.
+        - endvalue: The end value of the color change section.
+        - rgbs: A string representing the color channels to apply the color change section to.
+                Default is "rgb" (all color channels).
+        - intensity: The intensity of the color change section. Default is 1.0.
+        """
         if "r" in rgbs.lower():
             self.sequence[0].append([duration,Section(type, endvalue, intensity)])	
         if "g" in rgbs.lower():
@@ -15,6 +46,15 @@ class Sequence:
             self.sequence[2].append([duration,Section(type, endvalue, intensity)])
     
     def evaluate(self, time):
+        """
+        Evaluates the color values at a given time.
+
+        Parameters:
+        - time: The time point to evaluate the color values at.
+
+        Returns:
+        - A tuple representing the RGB color values at the given time.
+        """
         rgb = [0,0,0]
         total_time = time
         for i in range(3):
@@ -35,17 +75,44 @@ class Sequence:
         return tuple(rgb)
 
     def get_values(self, x):
+        """
+        Returns the color values for a list of time points.
+
+        Parameters:
+        - x: A list of time points.
+
+        Returns:
+        - A list of tuples representing the RGB color values at each time point.
+        """
         y = []
         for i in x:
             y.append(self.evaluate(i))
         return y
     
     def lines(self, x):
+        """
+        Returns a list of Scatter objects for plotting the color change lines.
+
+        Parameters:
+        - x: A list of time points.
+
+        Returns:
+        - A list of Scatter objects representing the color change lines.
+        """
         y = self.get_values(x)
         return [go.Scatter(x=x, y=[i[color] for i in y], mode='lines', 
                                         name=["red","green","blue"][color],line=dict(color=["red","green","blue"][color])) for color in range(3)]
 
     def plot(self, x):
+        """
+        Returns a Figure object for plotting the color change lines.
+
+        Parameters:
+        - x: A list of time points.
+
+        Returns:
+        - A Figure object representing the plot of the color change lines.
+        """
         y = self.get_values(x)
         fig = go.Figure()
         for color in range(3):
@@ -54,9 +121,28 @@ class Sequence:
         return fig
 
     def to_binary(self, rgb):
+        """
+        Converts the color values to binary format.
+
+        Parameters:
+        - rgb: A tuple representing the RGB color values.
+
+        Returns:
+        - A tuple representing the RGB color values in binary format.
+        """
         return tuple([int(i*255) for i in rgb])
 
     def create_frames(self, x, size):
+        """
+        Creates frames for animation.
+
+        Parameters:
+        - x: A list of time points.
+        - size: The size of the markers in the animation.
+
+        Returns:
+        - A list of Frame objects representing the frames for animation.
+        """
         frames = []
         rgb_values = self.get_values(x)
         for i, rgb in enumerate(rgb_values):
@@ -74,6 +160,15 @@ class Sequence:
         return frames
     
     def create_frame2(self, x):
+        """
+        Creates frames for the second subplot in the animation.
+
+        Parameters:
+        - x: A list of time points.
+
+        Returns:
+        - A list of Scatter objects representing the frames for the second subplot.
+        """
         rgb_values = self.get_values(x)
         frame = []
         for i, rgb in enumerate(rgb_values):
@@ -90,6 +185,17 @@ class Sequence:
         return frame
 
     def animate(self, x, fps=30,size=250):
+        """
+        Creates an animated plot of the color changes.
+
+        Parameters:
+        - x: A list of time points.
+        - fps: The frames per second for the animation. Default is 30.
+        - size: The size of the markers in the animation.
+
+        Returns:
+        - A Figure object representing the animated plot of the color changes.
+        """
         frames = self.create_frames(x, size)
         # Create layout
         layout = go.Layout(
@@ -114,6 +220,15 @@ class Sequence:
         return fig
 
     def show(self, x):
+        """
+        Creates a subplot with the color change lines and the animated plot.
+
+        Parameters:
+        - x: A list of time points.
+
+        Returns:
+        - A Figure object representing the subplot with the color change lines and the animated plot.
+        """
         fig = make_subplots(rows=1,cols=2)
         lines = self.lines(x)
         anim_fig = self.animate(x)
